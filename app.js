69e7e299e1c44b1a63a6fb6f93b8fff9a7bd448a
martin.calsina <martincalsina05@gmail.com>
2025-01-22 19:57:17 -0300

    la funcion agregarAmigo actualiza el template HTML exitósamente

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre.")
     } else {
         listaAmigos.push(nombreAmigo);
+        actualizarListaAmigos();
         limpiarInput();
+    }
+}
+
+//actualiza el template de HTML con un tag <li></li> por cada elemento que haya en listaAmigos
+function actualizarListaAmigos() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = ""; //reseteamos los li que hayan dentro de la etiqueta <ul>
+    for (let i=0; i<listaAmigos.length;i++) {
+        listaHTML.innerHTML += `<li>${listaAmigos[i]}</li>`; //agregamos un <li> por cada amigo guardado
     }
 }
 
