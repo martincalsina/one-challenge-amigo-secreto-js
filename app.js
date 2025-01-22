225a6bea4b84cee03e24ca292f1c41043e1a5034
martin.calsina <martincalsina05@gmail.com>
2025-01-22 19:50:07 -0300

    función agregarAmigo altera listaAmigos exitósamente

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creamos un arreglo para guardar a los amigos ingresados
+let listaAmigos = [];
+
+//verifica que el input del usuario no esté vacío y agregue el amigo a la lista en dicho caso
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo == "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        listaAmigos.push(nombreAmigo);
+        limpiarInput();
+    }
+}
+
+function limpiarInput() {
+    document.getElementById('amigo').value = "";
+}
